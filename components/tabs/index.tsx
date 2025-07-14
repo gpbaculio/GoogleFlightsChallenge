adf257f14abc7f76b00f15e1da1d1e939f1d2e94
Glendon Philipp Baculio <gpbaculio@gmail.com>
2025-07-14 16:32:42 +0800

    Add FormContainer component to tabs
    Introduces a new FormContainer component in the tabs module, using ViewBox for styled container layout. This component provides consistent styling for form sections within tabbed interfaces.

---
+++ components/tabs/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { ViewBox } from "../ui";
+
+export function FormContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <ViewBox
+      zIndex={2}
+      top={-1}
+      borderBottomLeftRadius={16}
+      borderBottomRightRadius={16}
+      width="100%"
+      pb="m"
+      backgroundColor="background"
+      shadowColor="onBackground"
+      shadowRadius={2}
+      elevation={2}
+      shadowOpacity={0.2}
+      shadowOffset={{ width: 0, height: 1 }}
+      px="s"
+    >
+      {children}
+    </ViewBox>
+  );
+}
