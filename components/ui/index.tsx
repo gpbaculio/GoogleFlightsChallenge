39b1b73068cbd617268eb15c423c344e12d94432
Glendon Philipp Baculio <gpbaculio@gmail.com>
2025-07-14 16:39:54 +0800

    Add TextInputBox component to UI index
    Introduced TextInputBox by creating a container for a themed TextInput component. This allows consistent styling and usage of TextInput across the application.

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -1,17 +1,22 @@
 import { createBox, createText } from "@shopify/restyle";
 import {
-    Image,
-    ImageProps,
-    TouchableOpacity,
-    TouchableOpacityProps,
-    View,
-    ViewProps,
+  Image,
+  ImageProps,
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+  ViewProps,
 } from "react-native";
 
 import { Theme } from "@/theme";
 import { createContainer } from "@/utils/createContainer";
 
 export const ViewBox = createContainer(createBox<Theme, ViewProps>(View));
+export const TextInputBox = createContainer(
+  createBox<Theme, TextInputProps>(TextInput)
+);
 export const TextBox = createText<Theme>();
 export const TouchableOpacityBox = createContainer(
   createBox<Theme, TouchableOpacityProps>(TouchableOpacity)
