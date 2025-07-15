8f763a95afceb8ebd83688d7581244f6bb15d28e
Glendon Philipp Baculio <gpbaculio@gmail.com>
2025-07-15 20:54:09 +0800

    Add animated view and pressable box components
    Introduces AnimatedViewBox and AnimatedPressableBox components using react-native-reanimated. These components allow for animated styling and interactions within the existing theming and container system.

--- components/index.tsx
+++ components/index.tsx
@@ -2,6 +2,7 @@ import { createBox, createText } from "@shopify/restyle";
 import {
   Image,
   ImageProps,
+  Pressable,
   TextInput,
   TextInputProps,
   TouchableOpacity,
@@ -12,13 +13,24 @@ import {
 
 import { Theme } from "@/theme";
 import { createContainer } from "@/utils/createContainer";
+import Animated from "react-native-reanimated";
 
 export const ViewBox = createContainer(createBox<Theme, ViewProps>(View));
+const AnimatedView = Animated.createAnimatedComponent(View);
+export const AnimatedViewBox = createContainer(
+  createBox<Theme, React.ComponentProps<typeof AnimatedView>>(AnimatedView)
+);
 export const TextInputBox = createContainer(
   createBox<Theme, TextInputProps>(TextInput)
 );
 export const TextBox = createText<Theme>();
 export const TouchableOpacityBox = createContainer(
   createBox<Theme, TouchableOpacityProps>(TouchableOpacity)
+);
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+export const AnimatedPressableBox = createContainer(
+  createBox<Theme, React.ComponentProps<typeof AnimatedPressable>>(
+    AnimatedPressable
+  )
 );
 export const ImageBox = createContainer(createBox<Theme, ImageProps>(Image));
